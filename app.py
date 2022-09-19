b9e98e19ee80a46c53a546de507c96f8414aae90
rokoworlds <roman.koropat@gmail.com>
2022-09-19 17:49:47 +0300

    procfile3

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from website import create_app
 
 
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+# if __name__ == "__main__":
+app = create_app()
+app.run(debug=True)
